e44c8a2b996a04a87a9a6941b87ff7bfa9236574
calimendoza <mbaivictor1@gmail.com>
2022-12-08 21:17:43 -0800

    play game

--- reduxRoad.js
+++ reduxRoad.js
@@ -32,3 +32,14 @@ const initialWagonState = {
         return state;
     }
   }
+
+  let wagon  =  reducer(undefined, {})
+console.log(wagon)
+wagon = reducer(wagon, {type: 'travel', payload: 1})
+console.log(wagon)
+wagon = reducer(wagon, {type: 'gather'})
+console.log(wagon)
+wagon = reducer(wagon, {type: 'tippedWagon'})
+console.log(wagon)
+wagon = reducer(wagon, {type: 'travel', payload: 3})
+console.log(wagon)
