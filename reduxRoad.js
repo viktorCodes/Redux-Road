b8334dfa982a909718fd14b46f68bec9453ea728
calimendoza <mbaivictor1@gmail.com>
2022-12-08 21:16:42 -0800

    initial state and reducer

--- reduxRoad.js
+++ reduxRoad.js
@@ -3,3 +3,32 @@ const initialWagonState = {
     distance: 0,
     days: 0
   };
+
+  const reducer = (state = initialWagonState, action) => {
+    switch(action.type){
+      case 'gather' : {
+        return {
+          ...state,
+          supplies: state.supplies + 15,
+          days: state.days + 1,
+        }
+      }
+      case 'travel' : {
+        return {
+          ...state,
+          supplies: state.supplies - 20*action.payload,
+          distance: state.distance + 10*action.payload,
+          days: state.days + action.payload,
+        }
+      }
+      case 'tippedWagon' : {
+        return {
+          ...state,
+          supplies: state.supplies - 30,
+          days: state.days + 1,
+        }
+      }
+      default:
+        return state;
+    }
+  }
